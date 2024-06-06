6583e6ad1cb87b527ea08b89cf01dda9d1788964
nilo <aldunate24@gmail.com>
2024-06-05 20:38:29 -0500

    app django v3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -142,8 +142,8 @@ MESSAGE_TAGS = {
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'aldunate24@gmail.com'
-EMAIL_HOST_PASSWORD = 'linkinp@rkNilo2017'
+EMAIL_HOST_USER = 'privado@gmail.com'
+EMAIL_HOST_PASSWORD = 'privado'
 EMAIL_USE_TLS = True
 
 # Default primary key field type
